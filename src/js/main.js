6c581beaa1e8c8db19d4c8cb20b0d33a20b784d8
Dimas Wihandono <daimessdn@gmail.com>
2021-03-25 16:26:37 +0700

    added landing page

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 // element selector
-// // platform element
+//// platform element
 const platform = document.querySelector("#platform");
 
-// // notification triggers elements
+//// notification triggers elements
 const notificationElement = document.querySelector("#notification"),
       messageElement      = document.querySelector("#message"),
       actionsElement      = document.querySelector("#actions");
 
-// // stats elements
+//// stats elements
 const moveStatsElement  = document.querySelector("#moves"),
   boxesLeftStatsElement = document.querySelector("#boxes-left"),
   levelStatsElement     = document.querySelector("#level");
@@ -59,6 +59,7 @@ function rotate(element) {
 
 // update game stats
 function updateStats() {
+  // check for the box remaining to solved
   const boxesNeedToSolved = currentGame.boxes.filter(box => {
     if (box.orientation !== "none-line") {
       if (box.orientation == "horizontal-line" ||
@@ -74,11 +75,7 @@ function updateStats() {
 
   if (currentGame.moves < 1) {
     if (boxesNeedToSolved.length == 0) {
-      triggerNotification("Level completed!",
-                          "You can go to next level.",
-                          `<button class="action-btn" onclick="nextLevel(currentGame)">Next Level</button>
-                           <button class="action-btn" onclick="restartGame(currentGame)">Restart Level</button>
-                           <button class="action-btn">Back to Main Menu</button>`);
+      triggerLevelCompleted();
     } else {
       triggerNotification("Out of moves!",
                           "You are out of moves! Please try again.",
@@ -87,11 +84,7 @@ function updateStats() {
     }
   } else {
     if (boxesNeedToSolved.length == 0) {
-      triggerNotification("Level completed!",
-                          "You can go to next level.",
-                          `<button class="action-btn" onclick="nextLevel(currentGame)">Next Level</button>
-                           <button class="action-btn" onclick="restartGame(currentGame)">Restart Level</button>
-                           <button class="action-btn">Back to Main Menu</button>`);
+      triggerLevelCompleted();
     }
   }
 
@@ -114,6 +107,7 @@ function restartGame(game) {
   prepareGame(currentGame);
 }
 
+// get all game levels through JSON
 function getGameLevels() {
   const xhr = new XMLHttpRequest();
 
@@ -123,12 +117,15 @@ function getGameLevels() {
   return JSON.parse(xhr.responseText);
 }
 
+// function to prepare the game session
 function prepareGame(game) {
+  // prepare platform dimension
   platform.style.width = `calc(${game.dimension.x * 5}em +
                                ${game.dimension.x * 6}px + 16px)`;
   platform.style.height = `calc(${game.dimension.y * 5}em +
                                ${game.dimension.y * 6}px + 16px)`;
   
+  // prepare the game box
   setTimeout(function() {
     renderBoxes(game.boxes);
 
@@ -138,6 +135,7 @@ function prepareGame(game) {
     });
   }, 300);
 
+  // update notification and stats
   triggerNotification();
   updateStats();
 }
@@ -148,8 +146,21 @@ function nextLevel(game) {
     return gameLevel.level === (game.level + 1);
   })[0];
 
-  currentGame = nextGame;
+  currentGame = JSON.parse(JSON.stringify(nextGame));
   prepareGame(nextGame);
+
+  localStorage.setItem(CURRENTSAVE_LOCAL_KEY, JSON.stringify(currentGame));
+  currentSave = JSON.parse(localStorage.getItem(CURRENTSAVE_LOCAL_KEY));
+}
+
+// function to proceed game to next level
+function restartFromBeginning() {
+  currentGame = JSON.parse(JSON.stringify(gameLevels[0]));
+  console.log(currentGame);
+  prepareGame(currentGame);
+
+  triggerNotification();
+  updateStats();
 
   localStorage.setItem(CURRENTSAVE_LOCAL_KEY, JSON.stringify(currentGame));
   currentSave = JSON.parse(localStorage.getItem(CURRENTSAVE_LOCAL_KEY));
@@ -170,19 +181,47 @@ function triggerNotification(heading = "", message = "", actions = "") {
   actionsElement.innerHTML = actions;
 }
 
+function triggerLevelCompleted() {
+  if (currentGame.level == gameLevels.length) {
+    triggerNotification("You've completed all levels!",
+                        "Congratulations! Let's share your accomplishment on social media",
+                        `<button class="action-btn" onclick="restartFromBeginning()">Play again...</button>
+                         <button class="action-btn" onclick="restartGame(currentGame)">Restart Level</button>
+                         <button class="action-btn" onclick="shareOnFacebook()"><i class="fab fa-facebook"></i> Share</button>
+                         <button class="action-btn" onclick="shareOnTwitter()"><i class="fab fa-twitter"></i> Share</button>
+                         <button class="action-btn" onclick="shareOnLinkedIn()"><i class="fab fa-linkedin"></i> Share</button>
+                         <button class="action-btn">Back to Main Menu</button>`);
+  } else {
+    triggerNotification("Level completed!",
+                        "You can go to next level.",
+                        `<button class="action-btn" onclick="nextLevel(currentGame)">Next Level</button>
+                         <button class="action-btn" onclick="restartGame(currentGame)">Restart Level</button>
+                         <button class="action-btn" onclick="shareOnFacebook()"><i class="fab fa-facebook"></i> Share</button>
+                         <button class="action-btn" onclick="shareOnTwitter()"><i class="fab fa-twitter"></i> Share</button>
+                         <button class="action-btn" onclick="shareOnLinkedIn()"><i class="fab fa-linkedin"></i> Share</button>
+                         <button class="action-btn">Back to Main Menu</button>`);
+  }
+}
+
 // function for sharing on social media
+//// Facebook
+function shareOnFacebook() {
+  window.open("https://www.facebook.com/sharer/sharer.php?u=daimessdn.github.io/path-thern",
+              "_blank");
+}
+
+//// Twitter
 function shareOnTwitter() {
   window.open("https://twitter.com/share?url=daimessdn.github.io/path-thern&text=I just completed a level in path-thern. Let's play! ",
               "_blank");
 }
-
-function shareToLinkedIn() {
+//// LinkedIn
+function shareOnLinkedIn() {
   window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`, "_blank");
 }
 
+// LET THE GAME BEGIN!!!
 prepareGame(currentGame);
-
 triggerNotification("Welcome to path-thern.",
                       "Guide the alien to spaceship by rotating the tiles and create the right path.");
-setTimeout(function() { triggerNotification(); }, 10000);
 // skipLevel(19)
